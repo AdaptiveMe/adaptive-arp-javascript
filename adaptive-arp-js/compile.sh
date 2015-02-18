d377cafcb816f3c12ca856e7d5b336ebb8052f08
Carlos Lozano <carlos@adaptive.me>
2015-02-18 20:10:17 +0100

    Codestyle - enforce curly braces.

--- adaptive-arp-js/compile.sh
+++ adaptive-arp-js/compile.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "Post-processing TypeScript code."
+# npm install replace -g required!
 HOME_DIR=`pwd`
 cd $HOME_DIR/src_global
 tsc -d -m commonjs --sourcemap --target ES6 Adaptive.ts
+replace "for \(var p in b\) if \(b.hasOwnProperty\(p\)\) d\[p] = b\[p];" "for (var p in b) { if (b.hasOwnProperty(p)) { d[p] = b[p]; } }" *.js
 cd $HOME_DIR/src_units
 rm -rf *.d.ts
 tsc -d -m commonjs --sourcemap --target ES6 *.ts
+replace "for \(var p in b\) if \(b.hasOwnProperty\(p\)\) d\[p] = b\[p];" "for (var p in b) { if (b.hasOwnProperty(p)) { d[p] = b[p]; } }" *.js
--- adaptive-arp-js/src_units/ContactPersonalInfo.js
+++ adaptive-arp-js/src_units/ContactPersonalInfo.js
@@ -32,7 +32,7 @@ Release:
 -------------------------------------------| aut inveniam viam aut faciam |--------------------------------------------
 */
 var __extends = this.__extends || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    for (var p in b) { if (b.hasOwnProperty(p)) { d[p] = b[p]; } }
     function __() { this.constructor = d; }
     __.prototype = b.prototype;
     d.prototype = new __();
